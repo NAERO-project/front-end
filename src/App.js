fe9df1ba7127087a660e977e6241cf0995fb510d
soyuul <syuul3139@gmail.com>
2024-11-18 15:05:07 +0900

    main product branch merge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Main from "./pages/Main";
+// import Login from "./pages/user/Login";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route index element={<Main />} />
+
+					<Route path='product'>{/* 상품관련 페이지 */}</Route>
+					<Route path='mypage'>{/* 회원들의 마이페이지 */}</Route>
+
+					<Route path='producer'>{/* 판매자 페이지 */}</Route>
+
+					<Route path='management'>{/* 관리자들의 관리페이지 */}</Route>
+
+					{/* <Route path='login' element={<Login />} /> */}
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
---
+++ src/utils/tokenUtils.js
@@ -0,0 +1,8 @@
+//토큰 복호화
+import jwtDecode from "jwt-decode";
+
+export function decodeJwt(token) {
+	if (token === null) return null;
+
+	return jwtDecode(token);
+}
