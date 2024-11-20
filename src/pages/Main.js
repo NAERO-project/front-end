f7f8c5284f7303fac7f0c0c1a61917944f339db0
soyuul <syuul3139@gmail.com>
2024-11-20 15:35:21 +0900

    fix: header 홈으로 가는 로고 삽입

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,7 @@
+import Global from "../components/common/Global.module.css";
+
 function Main(props) {
-	return <div>메인 페이지</div>;
+	return <div className={Global}>메인 페이지</div>;
 }
 
 export default Main;
