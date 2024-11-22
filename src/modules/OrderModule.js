a57e38e3274e56ed9e49280e0df58cc3ffd2bc14
scho123 <nvmsnach@gmail.com>
2024-11-22 13:19:12 +0900

    feat: 주문 화면 주소 api 작업중

--- src/modules/OrderModule.js
+++ src/modules/OrderModule.js
@@ -1,18 +1,32 @@
 import { createActions, handleActions } from "redux-actions";
 
-const initialState = {};
+const initialState = {
+    cartItems: [],
+};
 
-export const GET_ORDER_PAGE = 'order/GET_ORDER_PAGE';   // 주문 페이지 정보 조회
+export const GET_ORDER_PAGE = "order/GET_ORDER_PAGE";
+export const POST_ORDER = "order/POST_ORDER";
+export const SET_CART_ITEMS = "order/SET_CART_ITEMS"; // 장바구니 데이터 저장 액션
 
 const actions = createActions({
-    [GET_ORDER_PAGE]: () => {}
+    [GET_ORDER_PAGE]: () => {},
+    [POST_ORDER]: () => {},
+    [SET_CART_ITEMS]: () => {},
 });
 
-const orderReducer = handleActions({
-    [GET_ORDER_PAGE]: (state, {payload}) => {
-        return payload;
-    }
-}, initialState);
+const orderReducer = handleActions(
+    {
+        [GET_ORDER_PAGE]: (state, { payload }) => {
+            return { ...state, ...payload };
+        },
+        [POST_ORDER]: (state, { payload }) => {
+            return { ...state, ...payload };
+        },
+        [SET_CART_ITEMS]: (state, { payload }) => {
+            return { ...state, cartItems: payload };
+        },
+    },
+    initialState
+);
 
 export default orderReducer;
-
