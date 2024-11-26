870ca3498b7f4f128e4568305a98957ad9479b96
Mana Li <yanhongli86@gmail.com>
2024-11-26 09:47:06 +0900

    feat: 송장입력을 위한 머지

--- src/pages/producer/ProducerOrderProductList.js
+++ src/pages/producer/ProducerOrderProductList.js
@@ -5,53 +5,50 @@ import { decodeJwt } from "../../utils/tokenUtils";
 
 import { callProducerOrderProductListApi } from "../../apis/OrderApiCall";
 
+import DeliverUpdateModal from "../../components/modal/DeliverUpdateModal";
+
 function ProducerOrderProductList({ orderId }) {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const isLogin = window.localStorage.getItem("accessToken"); // Local Storage 에 token 정보 확인
-    const producerUsername = isLogin ? decodeJwt(isLogin).sub : null; // JWT에서 사용자 ID 추출
+  const isLogin = window.localStorage.getItem("accessToken"); // Local Storage 에 token 정보 확인
+  const producerUsername = isLogin ? decodeJwt(isLogin).sub : null; // JWT에서 사용자 ID 추출
 
-    const orderProducts = useSelector(
-        (state) => state.orderReducer.producerProducts
-    );
+  const orderProducts = useSelector(
+    (state) => state.orderReducer.producerProducts
+  );
 
-    useEffect(() => {
-        dispatch(
-            callProducerOrderProductListApi({ orderId, producerUsername })
-        );
-        console.log("왜 안나오니...", orderProducts);
-    }, [dispatch, orderId]);
+  useEffect(() => {
+    dispatch(callProducerOrderProductListApi({ orderId, producerUsername }));
+    console.log("왜 안나오니...", orderProducts);
+  }, [dispatch, orderId]);
 
-    if (!orderProducts || !Array.isArray(orderProducts)) {
-        return <div>로딩중...</div>;
-    }
+  if (!orderProducts || !Array.isArray(orderProducts)) {
+    return <div>로딩중...</div>;
+  }
 
-    return (
-        <div className="">
-            {orderProducts.length > 0 ? (
-                orderProducts.map((product) => (
-                    <div className="" key={product.optionId}>
-                        <img src={product.productImg} alt="주문상품 이미지" />
-                        <p>상품 이름: {product.productName}</p>
-                        <p>
-                            상품 금액: {product.amount.toLocaleString("ko-KR")}
-                            원
-                        </p>
-                        <p>
-                            주문 수량: {product.count.toLocaleString("ko-KR")}
-                        </p>
-                    </div>
-                ))
-            ) : (
-                <div>상품이 없습니다.</div>
-            )}
-            <div className="">
-                <button onClick={() => {}}>송장등록</button><br/><br/>
-                <button onClick={() => {}}>배송 조회</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="">
+      {orderProducts.length > 0 ? (
+        orderProducts.map((product) => (
+          <div className="" key={product.optionId}>
+            <img src={product.productImg} alt="주문상품 이미지" />
+            <p>상품 이름: {product.productName}</p>
+            <p>상품 금액: {product.amount.toLocaleString("ko-KR")}원</p>
+            <p>주문 수량: {product.count.toLocaleString("ko-KR")}</p>
+          </div>
+        ))
+      ) : (
+        <div>상품이 없습니다.</div>
+      )}
+      <div className="">
+        <button onClick={() => {}}>송장등록</button>
+        <br />
+        <br />
+        <button onClick={() => {}}>배송 조회</button>
+      </div>
+    </div>
+  );
 }
 
 export default ProducerOrderProductList;
