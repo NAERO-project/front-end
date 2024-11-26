613790931b1936096d11a162b5141955d2946701
scho123 <nvmsnach@gmail.com>
2024-11-27 02:59:05 +0900

    fix: 로그인 만료 예외처리 로직 일부 적용, 주문 완료 시 장바구니 상품 삭제 기능, 주문 완료 후 페이지 이동

---
+++ src/components/common/RequireAuth.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { decodeJwt } from "../../utils/tokenUtils";
+
+// 로그인을 해야 접근이 가능한 페이지는 토큰 만료 시 로그인 화면으로 이동 시키는 로직
+const RequireAuth = ({ children }) => {
+    const navigate = useNavigate();
+    const isLogin = window.localStorage.getItem("accessToken");
+    const decodedToken = decodeJwt(isLogin);
+
+    useEffect(() => {
+        if (!isLogin || !decodedToken || isTokenExpired(decodedToken)) {
+            alert("로그인 세션이 만료되었습니다. 다시 로그인 해주세요.");
+            navigate("/login");
+        }
+    }, [isLogin, decodedToken, navigate]);
+
+    if (!isLogin || !decodedToken || isTokenExpired(decodedToken)) {
+        return null; // 리다이렉트 중에는 아무것도 렌더링하지 않음
+    }
+
+    return children;
+};
+
+function isTokenExpired(decodedToken) {
+    const currentTime = Math.floor(Date.now() / 1000);
+    return decodedToken.exp < currentTime;
+}
+
+export default RequireAuth;
